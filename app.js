673b757dbaf28c1ad54a284e82f896f8057f1ee6
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-20 22:40:22 +0600

    Updated friends array practice

--- app.js
+++ app.js
@@ -10,21 +10,27 @@ if (num >= 20 && num <= 30) {
   console.log("Nooooo");
 }
 const friends = ["hero", "jrk", "srk", "rahim"];
-console.log(friends[1]);
+// console.log(friends[1]);
+friends.push("BD")
+friends.pop()
+friends.unshift("Bd")
+friends.shift()
+console.log(friends.slice(2,3));
+console.log(friends.splice(2,3));
 
 //
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-let sum = a + b;
+// let sum = a + b;
 
-console.log(sum);
-const numbers = [10, 20, 30, 40];
+// console.log(sum);
+// const numbers = [10, 20, 30, 40];
 
-let total = 0;
+// let total = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-  total = total + numbers[i];
-}
+// for (let i = 0; i < numbers.length; i++) {
+//   total = total + numbers[i];
+// }
 
-console.log(total);
+// console.log(total);
