cad0d864070fc60f9bbaf6d5955644fc05f5790d
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-20 22:27:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let name = "Muyed";
+name = "Muyedur Rahman";
+
+// if else
+
+const num = 40;
+if (num >= 20 && num <= 30) {
+  console.log("YES");
+} else {
+  console.log("Nooooo");
+}
+const friends = ["hero", "jrk", "srk", "rahim"];
+console.log(friends[1]);
+
+//
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+
+console.log(sum);
