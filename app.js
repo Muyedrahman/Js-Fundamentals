40c749eaea5525cd7ddf84c3f3afaec131701e9f
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-21 14:10:51 +0600

    for lup, Function,  object

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 let name = "Muyed";
 name = "Muyedur Rahman";
-
-// if else
-
-const num = 40;
-if (num >= 20 && num <= 30) {
-  console.log("YES");
-} else {
-  console.log("Nooooo");
-}
-const friends = ["hero", "jrk", "srk", "rahim"];
-// console.log(friends[1]);
-friends.push("BD")
-friends.pop()
-friends.unshift("Bd")
-friends.shift()
-console.log(friends.slice(2,3));
-console.log(friends.splice(2,3));
-
-//
 // let a = 10;
 // let b = 20;
 
@@ -34,3 +15,44 @@ console.log(friends.splice(2,3));
 // }
 
 // console.log(total);
+
+// if else
+
+const num = 40;
+if (num >= 20 && num <= 30) {
+//   console.log("YES");
+} else {
+//   console.log("Nooooo");
+}
+// const friends = ["hero", "jrk", "srk", "rahim"];
+// console.log(friends[1]);
+// friends.push("BD")
+// friends.pop()
+// friends.unshift("Bd")
+// friends.shift()
+// console.log(friends.slice(2,3));
+// console.log(friends.splice(2,3));
+
+// for lup
+for (let i = 0; i < friends.length; i++){
+    const element =friends[i]
+    console.log(element)
+}
+
+// Function
+function sum (a,b){
+    const result = a + b;
+    return result;
+}
+const output = sum(4, 5);
+console.log(output);
+
+// object
+const friends = ["hero", "jrk", "srk", "rahim"];
+const person = {
+    name: "muyrd",
+    age: 10,
+    friends: friends
+}
+console.log(person.friends[1])
+
